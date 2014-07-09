e70fe57a1df4eeb4bbc7965acbb0294da6113a5d
jorgeer <jfemail3@gmail.com>
2014-07-09 17:58:45 +0200

    beginning calculations

--- brewing.js
+++ brewing.js
@@ -19,30 +19,32 @@ nextPage = function (page){
     });
 };
 
+formToObject = function (form) {
+    form = $(form);
+    var o = {};
+    var a = form.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
 
 
 UI.registerHelper("malts", function() {
-    //return Malt.find();
-    /*
-    var malts = [{
-        name: "Supermalt",
-        ebc: 11,
-        gravity: 11111
-    },
-    {
-        name: "Defaultmalt",
-        ebc: 1,
-        gravity: 10000
-    }];
-    */
-    var malts = Malt.find().fetch();
-
-    malts = _.map(malts, function (malt, i) {
-        malt.label = malt.name + "    EBC: " + malt.ebc + "    Tetthet: " + malt.gravity;
+    return _.map(Malt.find().fetch(), function (malt, i) {
+        malt.label = malt.name +
+            "    EBC: " + malt.ebc +
+            "    Tetthet: " + malt.gravity;
         malt.value = i;
         return malt;
     });
-    return malts;
 });
 
 UI.registerHelper("hops", function() {
@@ -73,22 +75,57 @@ if (Meteor.isClient) {
 
     AutoForm.setDefaultTemplateForType('afQuickField', 'bootstrap3-horizontal');
 
+    Template.main.brewer = function () {
+        var userid = Session.get("user");
+        if (userid)
+            return Brewer.findOne(userid);
+        return false;
+    };
+
     Template.frontpage.events({
         "click .new-brew-btn": function (e) {
             nextPage("newbrew");
-
-
         }
     });
 
+    Template.frontpage.activeBrews = function () {
+        return Brew.find({ brewer: Session.get("user"), dateCompleted: { $exists: false } });
+    };
+
+    Template.newbrew.calculate = function (form) {
+        var data = formToObject(form);
+
+        console.log(data);
+    };
+
     Template.newbrew.events({
-        "change input[name='volume']": function (e) {
-            var $el = $(e.currentTarget),
-                val = $el.val();
-
-            console.log(val);
-
+        "change input": function (e) {
+            Template.newbrew.calculate("#insertBrewForm");
         },
+    });
+
+    AutoForm.hooks({
+        insertBrewerForm: {
+            after: {
+                insert: function(error, result, template) {
+                    if (!error) {
+                        console.log(result);
+                        Session.set("user", result);
+                        localStorage["user"] = result;
+                        nextPage("frontpage");
+                    } else {
+                        alert(error);
+                    }
+                    
+                },
+            }
+        }
+    });
+
+     Meteor.startup(function () {
+        if (localStorage["user"])
+            Session.set("user", localStorage["user"]);
+        Deps.flush();
     });
 }
 
